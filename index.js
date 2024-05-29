1a9b7b2a412f68a5e7c54578c5d36a1c33402547
jsgumban <gumban.johnhel@gmail.com>
2024-05-29 21:34:18 +0800

    Temporarily delete accounts

--- index.js
+++ index.js
@@ -16,58 +16,58 @@ mongoose.connect(uri)
 
 
 // Define a simple schema and model
-const AccountSchema = new mongoose.Schema({
-	accountGroup: String,
-	accountName: String,
-	accountAmount: Number,
-	accountDescription: String,
-});
-const Account = mongoose.model('accounts', AccountSchema );
-
-// CRUD routes here
-
-app.listen(port, () => {
-	console.log(`Server running on port ${port}`);
-});
-
-app.post('/accounts', async (req, res) => {
-	const newAccount = new Account(req.body);
-	try {
-		const savedAccount = await newAccount.save();
-		res.status(201).json(savedAccount);
-	} catch (err) {
-		res.status(400).json(err);
-	}
-});
-
-
-app.get('/accounts', async (req, res) => {
-	try {
-		const accounts = await Account.find();
-		res.json(accounts);
-	} catch (err) {
-		res.status(500).json(err);
-	}
-});
-
-app.put('/accounts/:id', async (req, res) => {
-	try {
-		const updatedAccount = await Account.findByIdAndUpdate(req.params.id, req.body, { new: true });
-		res.json(updatedAccount);
-	} catch (err) {
-		res.status(400).json({ message: err.message });
-	}
-});
-
-app.delete('/accounts/:id', async (req, res) => {
-	try {
-		await Account.findByIdAndDelete(req.params.id);
-		res.json({ message: 'Deleted Successfully' });
-	} catch (err) {
-		res.status(500).json({ message: err.message });
-	}
-});
-
+// const AccountSchema = new mongoose.Schema({
+// 	accountGroup: String,
+// 	accountName: String,
+// 	accountAmount: Number,
+// 	accountDescription: String,
+// });
+// const Account = mongoose.model('accounts', AccountSchema );
+//
+// // CRUD routes here
+//
+// app.listen(port, () => {
+// 	console.log(`Server running on port ${port}`);
+// });
+//
+// app.post('/accounts', async (req, res) => {
+// 	const newAccount = new Account(req.body);
+// 	try {
+// 		const savedAccount = await newAccount.save();
+// 		res.status(201).json(savedAccount);
+// 	} catch (err) {
+// 		res.status(400).json(err);
+// 	}
+// });
+//
+//
+// app.get('/accounts', async (req, res) => {
+// 	try {
+// 		const accounts = await Account.find();
+// 		res.json(accounts);
+// 	} catch (err) {
+// 		res.status(500).json(err);
+// 	}
+// });
+//
+// app.put('/accounts/:id', async (req, res) => {
+// 	try {
+// 		const updatedAccount = await Account.findByIdAndUpdate(req.params.id, req.body, { new: true });
+// 		res.json(updatedAccount);
+// 	} catch (err) {
+// 		res.status(400).json({ message: err.message });
+// 	}
+// });
+//
+// app.delete('/accounts/:id', async (req, res) => {
+// 	try {
+// 		await Account.findByIdAndDelete(req.params.id);
+// 		res.json({ message: 'Deleted Successfully' });
+// 	} catch (err) {
+// 		res.status(500).json({ message: err.message });
+// 	}
+// });
+//
 
 const billRoutes = require('./routes/bills');
 app.use('/bills', billRoutes);
