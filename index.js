47435792badfba48ad921cb9e4c0fbc9e49a4a3b
John Hel Gumban <>
2024-10-28 16:57:30 +0800

    Add websocket

--- index.js
+++ index.js
@@ -1,75 +1,63 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const http = require('http');
+const socketIo = require('socket.io');
 const cors = require('cors');
 
 require('dotenv').config();
+
 const app = express();
 const port = process.env.PORT || 5001;
 
 app.use(cors());
 app.use(express.json());
 
+// MongoDB connection
 const uri = "mongodb+srv://jsgumban:kgjIWNb69vreeprO@cluster0.tw8esq3.mongodb.net/finance_app?retryWrites=true&w=majority";
 mongoose.connect(uri)
 	.then(async () => {
-		console.log('MongoDB Connected')
+		console.log('MongoDB Connected');
 	})
 	.catch(err => console.log(err));
 
-
-// Define a simple schema and model
-// const AccountSchema = new mongoose.Schema({
-// 	accountGroup: String,
-// 	accountName: String,
-// 	accountAmount: Number,
-// 	accountDescription: String,
-// });
-// const Account = mongoose.model('accounts', AccountSchema );
-//
-// CRUD routes here
-
-app.listen(port, () => {
-	console.log(`Server running on port ${port}`);
+// HTTP server and Socket.IO setup
+const server = http.createServer(app);
+const io = socketIo(server, {
+	cors: {
+		origin: ['http://localhost:3000', 'http://192.168.0.44:3000'],
+		methods: ['GET', 'POST'],
+		credentials: true
+	}
 });
-//
-// app.post('/accounts', async (req, res) => {
-// 	const newAccount = new Account(req.body);
-// 	try {
-// 		const savedAccount = await newAccount.save();
-// 		res.status(201).json(savedAccount);
-// 	} catch (err) {
-// 		res.status(400).json(err);
-// 	}
-// });
-//
-//
-// app.get('/accounts', async (req, res) => {
-// 	try {
-// 		const accounts = await Account.find();
-// 		res.json(accounts);
-// 	} catch (err) {
-// 		res.status(500).json(err);
-// 	}
-// });
-//
-// app.put('/accounts/:id', async (req, res) => {
-// 	try {
-// 		const updatedAccount = await Account.findByIdAndUpdate(req.params.id, req.body, { new: true });
-// 		res.json(updatedAccount);
-// 	} catch (err) {
-// 		res.status(400).json({ message: err.message });
-// 	}
-// });
-//
-// app.delete('/accounts/:id', async (req, res) => {
-// 	try {
-// 		await Account.findByIdAndDelete(req.params.id);
-// 		res.json({ message: 'Deleted Successfully' });
-// 	} catch (err) {
-// 		res.status(500).json({ message: err.message });
-// 	}
-// });
-//
 
+// Socket.IO connection handler
+io.on('connection', (socket) => {
+	console.log('A user connected');
+	
+	// Handle moveCard events from clients
+	socket.on('moveCard', (data) => {
+		const { fromListId, toListId, cardId } = data;
+		console.log(`Card ${cardId} moved from List ${fromListId} to List ${toListId}`);
+		
+		// Broadcast the moveCard event to other clients
+		socket.broadcast.emit('cardMoved', data);
+	});
+	
+	socket.on('disconnect', () => {
+		console.log('A user disconnected');
+	});
+});
+
+// Basic API endpoint
+app.get('/', (req, res) => {
+	res.send('API is running');
+});
+
+// Import routes
 const billRoutes = require('./routes/bills');
 app.use('/bills', billRoutes);
+
+// Start the server on port 5001 (or specified port) and allow access from network
+server.listen(port, '0.0.0.0', () => {
+	console.log(`Server running on port ${port}`);
+});
